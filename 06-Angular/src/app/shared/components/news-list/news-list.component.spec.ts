a5dda9383e16ebcef38919d45bcfbf8b1dab5dc2
ruslan.abdraev <ruslan.abdraev@havi.com>
2020-02-12 00:38:43 +0300

    Implemented tests for NewsItemComponent and NewsListComponent

--- 06-Angular/src/app/shared/components/news-list/news-list.component.spec.ts
+++ 06-Angular/src/app/shared/components/news-list/news-list.component.spec.ts
@@ -1,15 +1,24 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NewsListComponent } from './news-list.component';
-import {Component} from '@angular/core'
+import { Input, Component, NO_ERRORS_SCHEMA} from '@angular/core'
+import { By } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-news-item',
   template: '<p>Mock News Item</p>'
 })
-class MockNewsItemComponent {}
-
-
+class MockNewsItemComponent {
+  @Input() id:string;
+  @Input() author:string;
+  @Input() title:string;
+  @Input() description:string;
+  @Input() url:string;
+  @Input() urlToImage: string;
+  @Input() publishedAt: string;
+  @Input() isLocal: boolean;
+}
 
 describe('NewsListComponent', () => {
   let component: NewsListComponent;
@@ -17,7 +26,8 @@ describe('NewsListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewsListComponent, MockNewsItemComponent ]
+      imports: [CommonModule, FormsModule],
+      declarations: [ NewsListComponent,  MockNewsItemComponent ],
     })
     .compileComponents();
   }));
@@ -28,7 +38,12 @@ describe('NewsListComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('has app-news-item element', () => {
+    const compiled = fixture.debugElement.query(By.css("app-news-item"));
+    expect(compiled).toBeDefined();
+  });
 });
